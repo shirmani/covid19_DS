eba35ab3780ab8c23f50aa7a75760dcde9c3144e
shirmani <smart060606@gmail.com>
2021-06-06 13:42:32 +0300

    Pexpaneion setup & init

---
+++ python_expansion/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="python_expansion",
+    version="0.0.1",
+    author="Shir Mani",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],)
