d4ec35e293b8f484a9f059d39de770373695facd
shirmani <smart060606@gmail.com>
2021-06-07 12:53:11 +0300

    a

--- programmerUI/programmerUI/display.py
+++ programmerUI/programmerUI/display.py
@@ -11,8 +11,8 @@ class Display:
     def buffer_with_num_of_line():
         print()
         print()
-        print(Display.num_of_line(2) + "-----------------------------")
+        print(Display.num_of_line(3) + "-----------------------------")
 
     @staticmethod
-    def print_with_num_of_line(string, depth=1):
+    def print_with_num_of_line(string, depth=2):
         print(Display.num_of_line(depth) + " " + string)
