48f7608927e45aae03f2ffbc8f580102378997c7
shirmani <smart060606@gmail.com>
2021-06-13 21:10:40 +0300

    write tool for help test clean feature

--- test_tool/tool_for_test.py
+++ test_tool/tool_for_test.py
@@ -2,6 +2,33 @@ import pandas as pd
 
 
 class Tool:
+
     @staticmethod
     def compare_dfs(dfa, dfb):
         return dfa.equals(dfb)
+
+    @staticmethod
+    def if_val_equal_0_return_True_else_False(val):
+        if val == 0:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def if_a_category_column_contains_only_valid_values(df, col, valid_values):
+        result = 0
+        for i in set(dict(df[col].value_counts()).keys()):
+            if i not in set(valid_values):
+                result += 1
+        return Tool.if_val_equal_0_return_True_else_False(result)
+
+    @staticmethod
+    def if_category_col_contains_only_valid_values_on_ls_of_dfs(dfs, col, valid_values):
+        result = 0
+        for df in dfs:
+            if col in df.columns:
+                if not Tool.if_a_category_column_contains_only_valid_values(df, col, valid_values):
+                    result += 1
+        return Tool.if_val_equal_0_return_True_else_False(result)
+
+
