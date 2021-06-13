e8cc211b90ef7d2fa2472e2d287da94dca51e492
shirmani <smart060606@gmail.com>
2021-06-13 21:14:07 +0300

    change funcs: add_comma_to_value_and_replace_null_with_empty_str, clean_text_col_from_punctuation, change_text_col_to_ls_words_col

--- clean_data/clean.py
+++ clean_data/clean.py
@@ -1,21 +1,19 @@
 import numpy as np
-from python_expansion import *
-from python_expansion.python_expansion import Pexpansion
+import pandas as pd
+from python_expansion_lib.python_expansion import Pexpansion
 
 
 class Clean:
     """
     """
     @staticmethod
-    def replace_value_by_comparison(df, name_input_col, replacement_dict, name_output_col=None): # clean_by_comparison
+    def replace_value_by_comparison(df, name_input_col, replacement_dict, name_output_col=None):
         """
         name_col_output: str
             if None output_col = col_input
 
         replacement_dict: ls
           {replacement_value: [ list of compare value], ..}
-
-        Impossible: {None: [np.nan]}
         """
         if not name_output_col:
             name_output_col = name_input_col
@@ -25,7 +23,7 @@ class Clean:
                 df.loc[df[name_input_col] == i, name_output_col] = k
 
     @staticmethod
-    def replace_value_by_contained_x(df, name_input_col, contained_dict, name_output_col=None): #clean_by_contained_values
+    def replace_value_by_contained_string(df, name_input_col, contained_dict, name_output_col=None): #clean_by_contained_values
         """
         name_col_output: str
             if None output_col = col_input
@@ -40,7 +38,7 @@ class Clean:
             for i in contained_dict[k]:
                 df.loc[df[name_input_col].astype(str).str.contains(i,  na=False, regex=False),
                        name_output_col] = k
-
+    # TODO: test
     @staticmethod
     def replace_value_by_contained_all_x_in_ls(df, name_input_col, contained_dict, name_output_col=None):
         """contained_dict = {replacment: [ls]}"""
@@ -54,32 +52,31 @@ class Clean:
                     df.loc[check_df[col] == 1, name_output_col] = k
 
     @staticmethod
-    def replace_empty_value_to_npnan(df, col):
-        col_type = df[col].dtypes
-        df[col] = df[col].astype("str")
-        df[col] = df[col][df[col].notnull()].apply(lambda x: x.strip())
-        df[col] = df[col].astype(col_type)
-        Clean.replace_value_by_comparison(df, col, {np.nan: ["", "nan", "None"]})
+    def replace_empty_value_to_npnan(dfs, col):
+        dfs = Pexpansion.if_x_not_ls_make_x_ls(dfs)
+        for df in dfs:
+            df[col] = df[col].fillna(np.nan)
+            df[col] = df[col][df[col].notnull()].apply(lambda x: Pexpansion.return_npnan_instead_empty_space(x))
 
     @staticmethod
     def clean_text_col_from_punctuation(df, col):
-        df[col] = df[col].apply(lambda x: Pexpansion.replace_str_by_comparison(x,
-                                {" ": [",", ".", ";", ":", "-", "‚", "+", "!", "_"]})
-                                if x == x else x)
+        punctuation = {" ": [",", ".", ";", ":", "-", "‚", "+", "!", "_", "?", "|", '/', ]}
+        df[col] = df[col].apply(lambda x: Pexpansion.replace_str_by_comparison(x, punctuation) if type(x) == str else x)
 
     @staticmethod
-    def add_comma_to_notnull_value_in_col(df, col):
+    def add_comma_to_value_and_replace_null_with_empty_str(df, col):
         Clean.replace_empty_value_to_npnan(df, col)
-        df.loc[df[col].notnull(), col] = df[col] + ','
+        df[col] = df[col].fillna("")
+        df[col] = df[col].astype(str) + ','
+        Clean.replace_value_by_comparison(df, col, {"": ","})
 
-    # test
     @staticmethod
-    def change_words_col_to_ls_word_col(df, col):
+    def change_text_col_to_ls_words_col(df, col):
         """ "abc abc abc" -> ["abc","abc", "abc"] """
         Clean.clean_text_col_from_punctuation(df, col)
         Clean.replace_empty_value_to_npnan(df, col)
         df[col] = df[col].apply(lambda x: x.split(" ") if type(x) == str else x)
-        df[col] = df[col].apply(lambda x: Pexpansion.remove_from_ls(x, ["", " "]) if type(x) == list else x)
+        df[col] = df[col].apply(lambda x: Pexpansion.remove_from_flat_ls(x, [""]) if type(x) == list else x)
 
     # test
     @staticmethod
