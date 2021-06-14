00047d53fc6dddf2f18c1385ab670115864a3867
shirmani <smart060606@gmail.com>
2021-06-14 19:47:14 +0300

    done treatment

--- clean_feature/clean_feature.py
+++ clean_feature/clean_feature.py
@@ -2,6 +2,7 @@ import numpy as np
 from clean_data.clean import Clean
 from clean_data.clean_time import CTime
 from clean_text.guess.guess_by_dict import GuessByDict
+from clean_text.organize_col.category_col import CategoryCol
 from clean_text.text_analysis import TextAnalysis
 from programmerUI.display import Display
 
@@ -34,12 +35,12 @@ class CleanJ:
     def background_diseases(self, background_diseases_bag_words, background_diseases_sentences_bag,
                             mexico_background_diseases_cols):
 
-        text_analysis_dict = {"vietnam": ["background_diseases_origin"],
+        df_to_background_diseases_text_analysis = {"vietnam": ["background_diseases_origin"],
                               "guatemala": ["background_diseases_origin"],
                               "world": ["symptoms_origin", "background_diseases_origin"],
                               "philippines":  ["Final Diagnosis", "background_diseases_origin"]}
 
-        for df_name, cols in text_analysis_dict.items():
+        for df_name, cols in df_to_background_diseases_text_analysis.items():
             TextAnalysis.text_analysis(self.store_df.get_df_by_name(df_name), cols,
                                        "background_diseases",
                                        GuessByDict(bag_words=background_diseases_bag_words,
@@ -66,3 +67,43 @@ class CleanJ:
 
         Clean.replace_value_by_comparison(self.usa, "background_diseases_binary",
                                           {1: ["Yes"], 0: ["No"], np.nan: ["Missing", "Unknown"]})
+
+    def treatment(self, world_treatment_bag_words, world_treatment_sentences_bag):
+        for df in [self.vietnam, self.singapore, self.philippines]:
+            Clean.replace_value_by_comparison(df, "treatment", {np.nan: ["For validation", "?"]})
+            df.loc[df["treatment"].notnull(), "treatment"] = "hospitalized"
+
+        for df in [self.kerla, self.india_data, self.hong_kong]:
+            Clean.replace_value_by_comparison(df, "severity_illness",
+                                              {"hospitalized": ["Hospitalized", "Hospitalised"]},
+                                              name_output_col="treatment")
+
+        #  france
+        Clean.replace_value_by_comparison(self.france, "treatment", {"hospitalized": ["released0", "hospital"],
+                                                                np.nan: ["deceased"]})
+
+        # colombia
+        Clean.replace_value_by_comparison(self.colombia, "treatment_origin",
+                                          {"hospitalized": ["Hospital Uci", "Hospital"],
+                                           "home isolation": ["Casa"]}, name_output_col="treatment")
+
+        # mexico
+        Clean.replace_value_by_comparison(self.mexico, "treatment", {"hospitalized": [2],
+                                                                "home isolation": [1]})
+
+        # toronto
+        for col in ["Ever in ICU", "Ever Intubated"]:
+            Clean.replace_value_by_comparison(self.toronto, col, {"hospitalized": ["Yes"]},
+                                              name_output_col="treatment")
+
+        # usa
+        for col in ["icu_yn", "hosp_yn"]:
+            Clean.replace_value_by_comparison(self.usa, col, {"hospitalized": ["Yes"],
+                                                              "home isolation": ["No"]},
+                                              name_output_col='treatment')
+
+        # world
+        TextAnalysis.text_analysis(self.world, ["origin_severity_illness", "symptoms_origin"], "treatment",
+                                   guess_type=GuessByDict(bag_words=world_treatment_bag_words,
+                                                          bag_sentences=world_treatment_sentences_bag),
+                                   col_type=CategoryCol({"hospitalized": 2, "clinic": 1, "home isolation": 0}))
