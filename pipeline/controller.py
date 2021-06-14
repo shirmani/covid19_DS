61b9935131695a7fa55f448f7677c7a0e28c2d95
shirmani <smart060606@gmail.com>
2021-06-14 19:48:02 +0300

    start to connect pipeline

---
+++ pipeline/controller.py
@@ -0,0 +1,17 @@
+
+import A_clean_and_unite_data
+# store = read_data(origin_dict)
+#
+# clean_data(store)
+#
+# ds = unite_data_to_1_ds(store)
+
+
+
+# data_QA(ds)
+
+
+#
+# jengineer(ds)
+#
+# push_data_to(ds, ["kaggle", "git"])
