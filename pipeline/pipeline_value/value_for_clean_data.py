a8016a7dd99539cc847ae5bb2faf84f5b56f4299
shirmani <smart060606@gmail.com>
2021-06-15 14:40:24 +0300

    unite all pipeline value (read clean qa ..)

--- pipeline/pipeline_value/value_for_clean_data.py
+++ pipeline/pipeline_value/value_for_clean_data.py
@@ -1,29 +1,5 @@
 import numpy as np
 
-origin_dict = {
-             "colombia": ["sebaxtian/covid19co", "covid19co.csv"],
-              "korea": ["kimjihoo/coronavirusdataset", "PatientInfo.csv"],
-              "japan": ["tsubasatwi/close-contact-status-of-corona-in-japan", "COVID-19_Japan_Mar_07th_2020.csv"],
-              "vietnam": ["nhntran/vietnam-covid19-patient-dataset", "Vietnam_COVID-19_patient_data_May10_2020.csv"],
-              "france": ["lperez/coronavirus-france-dataset", "patient.csv"],
-              "tunisia": ["ghassen1302/coronavirus-tunisia",  "Coronavirus_Tunisia_updated.csv"],
-              "guatemala": ["ncovgt2020/covid19-guatemala", "patients.csv"],
-              "mexico": ["lalish99/covid19-mx", "covid-19_general_MX.csv"],
-              "philippines": ["sundiver/covid19-philippines-edges",  "COVID-19 Philippines.xlsx", "Cases"],
-              "india_data": ["samacker77k/covid19india", "data.csv"],
-              "india_wiki" : ["karthikcs1/covid19-coronavirus-patient-list-karnataka-india", "Corona Covid19 Karnataka.csv"], #v
-              "new_zealand": ["madhavkru/covid19-nz", "COVID19-NZ-known-cases (updated 23-3.2020).csv"],
-              "hong_kong": ["teddyteddywu/covid-19-hong-kong-cases", "covid19case_full_17_08.csv"],
-              "kerla": ["baburajr/covid19inkerala", "PatientData.csv"],
-              "indonesia": ["ardisragen/indonesia-coronavirus-cases", "patient.csv"],
-              "singapore": ["rhodiumbeng/singapores-covid19-cases", "singapore_covid-19_sg_cases_updated-2020-04-14.csv"],
-              "toronto": ["divyansh22/toronto-covid19-cases", "COVID19 cases Toronto.csv"],
-              "canada_cases": ["ryanxjhan/coronaviruscovid19-canada", "cases.csv"],
-              "canada_dead": ["ryanxjhan/coronaviruscovid19-canada", "mortality.csv" ],
-              "usa": ["arashnic/covid19-case-surveillance-public-use-dataset", "COVID-19_Case_Surveillance_Public_Use_Data.csv"],
-              "world": ["shirmani/covid19-re", "06122020latestdata.csv"]
-    }
-
 change_cols_names_by_df = {"france": {"health": "severity_illness", "status": "treatment"},
 
                            "tunisia": {"case": "id",
