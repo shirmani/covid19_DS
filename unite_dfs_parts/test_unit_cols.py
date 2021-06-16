96a07bbf7b877b1fd85d863fdb6aae06f634c764
shirmani <smart060606@gmail.com>
2021-06-16 15:11:45 +0300

    start test for Unite

---
+++ unite_dfs_parts/test_unit_cols.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import pytest
+
+from test_tool.tool_for_test import Tool
+from unite_dfs_parts.unite_col import Unite
+
+
+class TestUnite:
+    @pytest.fixture(scope="function")
+    def df(self):
+        df = pd.DataFrame({"axg": [1, 3, 4],
+                           "b": [1, 3, 4],
+                           "nxg": [3, 3, 5],
+                           "ggg": [1, 3, 4],
+                           "xdg": [1, 3, 4]})
+
+        yield df
+
+    def test_unite_cols(self, df):
+        Unite.unite_cols(df, ['axg', 'nxg'], delete=True)
+        tatget = pd.DataFrame({"axg": ["1,3", "3", "4,5"],
+                               "b": [1, 3, 4],
+                               "ggg": [1, 3, 4],
+                               "xdg": [1, 3, 4]})
+        assert Tool.compare_dfs(tatget, df)
+
+    def test_unite_all_the_cols_that_contain_x(self, df):
+        Unite.unite_all_the_cols_that_contain_x(df, "xg", "new")
+        result = pd.DataFrame({"b": [1, 3, 4],
+                               "ggg": [1, 3, 4],
+                               "xdg": [1, 3, 4],
+                               "new": ["1,3", "3", "4,5"]})
+
+        assert Tool.compare_dfs(result, df)
+
+
+if __name__ == '__main__':
+    pytest.main()
