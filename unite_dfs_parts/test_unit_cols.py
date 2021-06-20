c83b16aa67c80fe2d8d41ee150bf25c74f1bb098
shirmani <smart060606@gmail.com>
2021-06-20 09:57:03 +0300

    small refactor

--- unite_dfs_parts/test_unit_cols.py
+++ unite_dfs_parts/test_unit_cols.py
@@ -18,20 +18,20 @@ class TestUnite:
 
     def test_unite_cols(self, df):
         Unite.unite_cols(df, ['axg', 'nxg'], delete=True)
-        tatget = pd.DataFrame({"axg": ["1,3", "3", "4,5"],
+        target = pd.DataFrame({"axg": ["1,3", "3", "4,5"],
                                "b": [1, 3, 4],
                                "ggg": [1, 3, 4],
                                "xdg": [1, 3, 4]})
-        assert Tool.compare_dfs(tatget, df)
+        assert Tool.compare_dfs(target, df)
 
     def test_unite_all_the_cols_that_contain_x(self, df):
         Unite.unite_all_the_cols_that_contain_x(df, "xg", "new")
-        result = pd.DataFrame({"b": [1, 3, 4],
+        target = pd.DataFrame({"b": [1, 3, 4],
                                "ggg": [1, 3, 4],
                                "xdg": [1, 3, 4],
                                "new": ["1,3", "3", "4,5"]})
 
-        assert Tool.compare_dfs(result, df)
+        assert Tool.compare_dfs(target, df)
 
 
 if __name__ == '__main__':
