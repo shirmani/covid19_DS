9dbd4e72ba584d2b68cf87c60d23b537d828a4ab
shirmani <smart060606@gmail.com>
2021-06-20 23:21:13 +0300

    important refactor

--- store_dfs_lib/display_store_df.py
+++ store_dfs_lib/display_store_df.py
@@ -3,13 +3,11 @@ from disply_code_clear.display import Display
 
 
 class DisplayStoreDF:
-    def __init__(self, class_store):
-        self.class_store = class_store
-        self.dfs = class_store.dfs
-        self.dfs_names = class_store.dfs_names
+    def __init__(self, dfs_store):
+        self.dfs_store = dfs_store
 
     def print_df_by_name(self, df_name, num_rows=5):
-        df = self.class_store.get_df_by_name(df_name)
+        df = self.dfs_store.get_df_by_name(df_name)
         pd.set_option('expand_frame_repr', True,
                       "display.max_rows", num_rows,
                       "display.max_columns", len(df),
@@ -20,7 +18,7 @@ class DisplayStoreDF:
         pd.reset_option('display.width')
 
     def print_all_dfs(self):
-        for df_name in self.dfs_names:
+        for df_name in self.dfs_store.dfs_names:
             print(df_name)
             self.print_df_by_name(df_name)
             print("-------------------------------")
@@ -28,30 +26,30 @@ class DisplayStoreDF:
     def print_col_values_by_dfs(self, col):
         Display.buffer_with_num_of_line()
         print("--" + col + "--")
-        for i in range(len(self.dfs)):
-            print("\n" + self.dfs_names[i])
-            if col in self.dfs[i].columns:
-                print(self.dfs[i][col].value_counts())
+        for i in range(len(self.dfs_store.dfs)):
+            print("\n" + self.dfs_store.dfs_names[i])
+            if col in self.dfs_store.dfs[i].columns:
+                print(self.dfs_store.dfs[i][col].value_counts())
 
     def print_shape_dfs(self):
         Display.buffer_with_num_of_line()
         print("Shape of Dfs")
         shapes = []
-        for i in range(len(self.dfs_names)):
-            print(self.dfs_names[i], self.dfs[i].shape)
-            shapes.append(self.dfs[i].shape[0])
+        for i in range(len(self.dfs_store.dfs_names)):
+            print(self.dfs_store.dfs_names[i], self.dfs_store.dfs[i].shape)
+            shapes.append(self.dfs_store.dfs[i].shape[0])
         print("\nsum " + str(sum(shapes)))
 
     def print_cols_by_df(self):
         Display.buffer_with_num_of_line()
-        for i in range(len(self.dfs_names)):
-            print(self.dfs_names[i] + " " + str(self.dfs[i].columns))
+        for i in range(len(self.dfs_store.dfs_names)):
+            print(self.dfs_store.dfs_names[i] + " " + str(self.dfs_store.dfs[i].columns))
             print()
 
     def print_cols_values_by_dfs(self):
         Display.buffer_with_num_of_line()
-        for i in range(len(self.dfs_names)):
-            for col in self.dfs[i].columns:
-                print(self.dfs_names[i] + " " + col)
-                print(self.dfs[i][col].value_counts())
+        for i in range(len(self.dfs_store.dfs_names)):
+            for col in self.dfs_store.dfs[i].columns:
+                print(self.dfs_store.dfs_names[i] + " " + col)
+                print(self.dfs_store.dfs[i][col].value_counts())
                 print()
