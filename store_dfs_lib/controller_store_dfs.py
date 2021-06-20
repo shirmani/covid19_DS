0ee2b358039ca48f5bd59fe99149680b242e437c
shirmani <smart060606@gmail.com>
2021-06-20 10:56:28 +0300

    build get_ls_of_dfs_that_contain_col and giving a referral to display df_store funcs

--- store_dfs_lib/controller_store_dfs.py
+++ store_dfs_lib/controller_store_dfs.py
@@ -1,4 +1,5 @@
 from store_dfs_lib.display_store_df import DisplayStoreDF
+from store_dfs_lib.get_df_by_filter import FilterStoreDF
 
 
 class ControllerStoreDFs:
@@ -6,6 +7,7 @@ class ControllerStoreDFs:
     def __init__(self, store_df):
         self.store_df = store_df
         self.display_store_df = DisplayStoreDF(store_df)
+        self.filter_store = FilterStoreDF(store_df)
 
     def remove(self):
         self.store_df.remove()
@@ -16,5 +18,29 @@ class ControllerStoreDFs:
     def get_df_by_name(self, df_name):
         return self.store_df.get_df_by_name(df_name)
 
+    # filter
+    def get_dfs_names_if_contain_col(self, col):
+        return self.filter_store.get_dfs_names_if_contain_col(col)
+
+    # display
     def print_df_by_name(self, df_name, num_rows):
-        self.display_store_df.print_df_by_name(df_name, num_rowsnum_rows)
+        self.display_store_df.print_df_by_name(df_name, num_rows)
+
+    def print_shape_dfs(self):
+        self.display_store_df.print_shape_dfs()
+
+    def print_cols_by_df(self):
+        self.display_store_df.print_cols_by_df()
+
+    def print_col_values_by_dfs(self, col):
+        self.display_store_df.print_col_values_by_dfs(col)
+
+    def print_cols_values_by_dfs(self):
+        self.display_store_df.print_cols_values_by_dfs()
+
+    # filter + store
+    def get_ls_of_dfs_that_contain_col(self, col):
+        dfs_names = self.get_dfs_names_if_contain_col(col)
+        return [self.get_df_by_name(i) for i in dfs_names]
+
+
