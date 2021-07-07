6437864dff03a36c967d677fcd60ea593171a665
shirmani <smart060606@gmail.com>
2021-07-07 11:40:26 +0300

    refactor

--- clean_text/guess/guess_by_dict.py
+++ clean_text/guess/guess_by_dict.py
@@ -13,32 +13,33 @@ class GuessByDict(Guess):
         self.bag_sentences = Pexpansion.upside_down_dictionary(bag_sentences)
         self.num_decision = num_decision
 
-    def _get_hotvec_of_value_from_col(self):
-        return pd.get_dummies(self.df[self.input_col])
-
-    def _build_guide_dict_to_guess_sentence(self, ls_phrase):
+    def _build_dict_key_phrase_with_category_value_num_of_word_that_point_on_cat(self, ls_phrase):
         guide_dict_to_guess_sentence = {}
         for j in ls_phrase:
             for root, cats in self.bag_sentences.items():
                 if root in j:
                     for category in cats:
-                        if j + "," + category in guide_dict_to_guess_sentence.keys():
-                            guide_dict_to_guess_sentence[j + "," + category] += 1
+                        if j + "|cat|" + category in guide_dict_to_guess_sentence.keys():
+                            guide_dict_to_guess_sentence[j + "|cat|" + category] += 1
                         else:
-                            guide_dict_to_guess_sentence[j + "," + category] = 1
+                            guide_dict_to_guess_sentence[j + "|cat|" + category] = 1
         return guide_dict_to_guess_sentence
 
     def _make_guess_sentence_col_by_guide_dict(self, df_hotvec_phrase, guide_dict_to_guess_sentence):
         df_hotvec_phrase["guess_sentence"] = ""
         for k, v in guide_dict_to_guess_sentence.items():
             if v > self.num_decision - 1:
-                ls = k.split(",")
+                ls = k.split("|cat|")
                 j, cat = ls[0], ls[1]
-                df_hotvec_phrase.loc[df_hotvec_phrase[j] == 1, "guess_sentence"] += cat + ","
+                try:
+                    df_hotvec_phrase.loc[df_hotvec_phrase[j] == 1, "guess_sentence"] += cat + ","
+                except KeyError:
+                    pass
         return df_hotvec_phrase.pop("guess_sentence")
 
     def _make_guess_sentence_col(self, df_hotvec_phrase):
-        guide_dict = GuessByDict._build_guide_dict_to_guess_sentence(self, df_hotvec_phrase.columns)
+        guide_dict = GuessByDict._build_dict_key_phrase_with_category_value_num_of_word_that_point_on_cat(
+            self, df_hotvec_phrase.columns)
         return GuessByDict._make_guess_sentence_col_by_guide_dict(self, df_hotvec_phrase, guide_dict)
 
     def _make_guess_word_col(self, df_hotvec_phrase):
@@ -58,7 +59,7 @@ class GuessByDict(Guess):
         return df
 
     def guess(self):
-        df_hotvec_phrase = self._get_hotvec_of_value_from_col()
+        df_hotvec_phrase = pd.get_dummies(self.df[self.input_col])
         sentence_col = self._make_guess_sentence_col(df_hotvec_phrase)
         word_col = self._make_guess_word_col(df_hotvec_phrase)
         return self._concat_word_sentence_guesses([word_col, sentence_col])
