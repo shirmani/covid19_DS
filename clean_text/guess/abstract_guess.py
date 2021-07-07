25b7755e6c31ab72a764babf25a858bf069814b1
shirmani <smart060606@gmail.com>
2021-07-07 11:40:08 +0300

    small change

--- clean_text/guess/abstract_guess.py
+++ clean_text/guess/abstract_guess.py
@@ -3,10 +3,10 @@ from abc import ABC, abstractmethod
 
 class Guess(ABC):
     @abstractmethod
-    def __init__(self, df=None, input_col=None, output_col_name=None):
+    def __init__(self, df=None, input_col="", output_col_name=""):
         self.df = df
-        self.input_col = input_col
-        self.output_col_name = output_col_name
+        self.input_col: str = input_col
+        self.output_col_name: str = output_col_name
 
     @abstractmethod
     def guess(self):
